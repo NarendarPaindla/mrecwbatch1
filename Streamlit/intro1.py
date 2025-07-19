6dc46781070c0b315e9c74054a2e9b79945cd662
NarendarTrainer <narendartrainer@gmail.com>
2025-07-19 10:28:14 +0530

    batch1

---
+++ Streamlit/intro1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title("Hi, i am Technical Trainer")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.text("This is a text")
+
+st.markdown("## heading2")
+st.markdown('[markdown website](https://www.markdownguide.org/cheat-sheet/)')
+table='''
+|Age|insurance|price|
+|---|---|-----|
+|25|no|25000|
+'''
+st.markdown(table)
