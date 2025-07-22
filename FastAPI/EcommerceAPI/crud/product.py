1b9c0d2ebc0fb4f0ad8bc02a873c0a213fd5cefe
NarendarTrainer <narendartrainer@gmail.com>
2025-07-22 16:09:58 +0530

    fastapi

--- FastAPI/EcommerceAPI/crud/product.py
+++ FastAPI/EcommerceAPI/crud/product.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from schemas.product import ProductCreate,Product
 from db.connection import get_db_connection
 
@@ -11,3 +12,38 @@ def create_product(payload: ProductCreate)-> Product:
     cursor.close()
     conn.close()
     return Product(id=new_id,**payload.dict())
+
+def list_products()->list[Product]:
+    conn=get_db_connection()
+    cursor=conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM products;")
+    rows=cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return [Product(**r) for r in rows]
+
+def update_product(product_id: int, payload: ProductCreate) -> Product:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    sql = ("UPDATE products SET name=%s, description=%s, price=%s "
+           "WHERE id=%s")
+    cursor.execute(sql, (payload.name, payload.description, payload.price, product_id))
+    if cursor.rowcount == 0:
+        conn.rollback()
+        raise HTTPException(status_code=404, detail="Product not found")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return Product(id=product_id, **payload.dict())
+
+
+def delete_product(product_id: int) -> None:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM products WHERE id = %s;", (product_id,))
+    if cursor.rowcount == 0:
+        conn.rollback()
+        raise HTTPException(status_code=404, detail="Product not found")
+    conn.commit()
+    cursor.close()
+    conn.close()
--- FastAPI/EcommerceAPI/routes/product.py
+++ FastAPI/EcommerceAPI/routes/product.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter,status
 from schemas.product import Product,ProductCreate
-from crud.product import create_product
+from crud.product import (create_product, delete_product,list_products, update_product)
 router=APIRouter(prefix="/products",tags=["products"])
 
 @router.post("",response_model=Product,status_code=status.HTTP_201_CREATED)
 def api_create_product(payload:ProductCreate):
     return create_product(payload)
+
+@router.get("",response_model=list[Product])
+def api_list_products():
+    return list_products()
+
+@router.put("/{product_id}", response_model=Product)
+def api_update_product(product_id: int, payload: ProductCreate):
+    return update_product(product_id, payload)
+
+@router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+def api_delete_product(product_id: int):
+    delete_product(product_id)
