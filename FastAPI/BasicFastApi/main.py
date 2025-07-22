517e028b2b26d9df30b9c7ceb4d36993ae903f78
NarendarTrainer <narendartrainer@gmail.com>
2025-07-22 11:16:46 +0530

    fastapi

--- FastAPI/BasicFastApi/main.py
+++ FastAPI/BasicFastApi/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 app=FastAPI()
 posts=[]
+
+class Post(BaseModel):
+    title:str
+    content:str
 @app.get("/")
 def root():
     return {"message":f"{posts}"}
 
 @app.post("/createpost")
-def create_post(hexa: dict= Body(...)):
-    posts.append(hexa)
-    return {f"post created, {hexa['title']},{hexa['content']}"}
+def create_post(post:Post):
+    post_dict=post.dict()
+    posts.append(post_dict)
+    return {"message":post_dict}
 
