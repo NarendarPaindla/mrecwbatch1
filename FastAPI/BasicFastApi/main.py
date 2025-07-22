8e8bbac20bddc0905701273a5c00b04432054849
NarendarTrainer <narendartrainer@gmail.com>
2025-07-22 11:01:11 +0530

    post created

--- FastAPI/BasicFastApi/main.py
+++ FastAPI/BasicFastApi/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 app=FastAPI()
-
+posts=[]
 @app.get("/")
 def root():
-    return {"message":"hello world"}
+    return {"message":f"{posts}"}
 
 @app.post("/createpost")
 def create_post(hexa: dict= Body(...)):
+    posts.append(hexa)
     return {f"post created, {hexa['title']},{hexa['content']}"}
 
