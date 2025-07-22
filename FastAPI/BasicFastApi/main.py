0e2f1f44e13d806f8fb49c0c1664a2d1539c12e5
NarendarTrainer <narendartrainer@gmail.com>
2025-07-22 10:53:42 +0530

    fastapi

--- FastAPI/BasicFastApi/main.py
+++ FastAPI/BasicFastApi/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-
+from fastapi.params import Body
 app=FastAPI()
 
 @app.get("/")
 def root():
     return {"message":"hello world"}
-@app.get("/about")
-def about():
-    return {"message":"This is about page"}
-@app.get("/contact")
-def contact():
-    return {"message":"This is contact page"}
+
+@app.post("/createpost")
+def create_post(hexa: dict= Body(...)):
+    return {f"post created, {hexa['title']},{hexa['content']}"}
+
